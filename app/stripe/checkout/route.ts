61a0f7f655b88e40ef22f6888dac2f7c8c825c48
sahangeesara <geesarasahan0123@gmail.com>
2026-06-07 01:31:05 +0530

    rote page issue deploy time

--- app/stripe/checkout/route.ts
+++ app/stripe/checkout/route.ts
@@ -1,23 +1,23 @@
-// import { stripe } from "@/lib/stripe";
-import { NextResponse } from "next/server";
+// // import { stripe } from "@/lib/stripe";
+// import { NextResponse } from "next/server";
 
-export async function POST() {
-  const session = await stripe.checkout.sessions.create({
-    mode: "subscription",
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        // price: process.env.STRIPE_PRICE_MONTHLY!,
-        quantity: 1,
-      },
-    ],
-    success_url:
-      "http://localhost:3000/subscription/success",
-    cancel_url:
-      "http://localhost:3000/subscription/cancel",
-  });
+// export async function POST() {
+//   const session = await stripe.checkout.sessions.create({
+//     mode: "subscription",
+//     payment_method_types: ["card"],
+//     line_items: [
+//       {
+//         // price: process.env.STRIPE_PRICE_MONTHLY!,
+//         quantity: 1,
+//       },
+//     ],
+//     success_url:
+//       "http://localhost:3000/subscription/success",
+//     cancel_url:
+//       "http://localhost:3000/subscription/cancel",
+//   });
 
-  return NextResponse.json({
-    url: session.url,
-  });
-}
+//   return NextResponse.json({
+//     url: session.url,
+//   });
+// }
